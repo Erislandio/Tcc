cb04f3ce11e9e7e51e1a64a0072bfb3192033223
Erislandio <erislandiosoares21@gmail.com>
2019-03-07 20:02:55 -0300

    Adição do logo

--- frontend/frontend/src/components/header/Header.jsx
+++ frontend/frontend/src/components/header/Header.jsx
@@ -10,12 +10,22 @@ class Header extends Component {
                 <Container>
                     <div className="header-content container">
                         <div className="logo">
-                            logo
+                            <a href="#">
+                                <img src="https://ronaldo-caetano.000webhostapp.com/Logo%20png.png" height="50" width="130" alt="SeEye" />
+                            </a>
                         </div>
                         <nav className="nav">
                             <ul>
                                 <li className="menu-link">Home</li>
-                                <li className="menu-link">Testes</li>
+                                <li className="menu-link">
+                                    Testes
+                                    <div className="dropdown-menu-1">
+                                        <ul>
+                                            <li>Camera</li>
+                                            <li>Imagem</li>
+                                        </ul>
+                                    </div>
+                                </li>
                                 <li className="menu-link">Contato</li>
                                 <li className="menu-link">Sobre</li>
                             </ul>
