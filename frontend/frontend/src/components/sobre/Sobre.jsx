cd7c0d90650999d253110138d4752f3b1e58afff
Ronaldo <ronaldogodoi2@gmail.com>
2019-04-10 22:10:02 -0300

    ajustes css no loading mobile

--- frontend/frontend/src/components/sobre/Sobre.jsx
+++ frontend/frontend/src/components/sobre/Sobre.jsx
@@ -8,20 +8,22 @@ class Sobre extends Component {
                 <h1 className="sobre-title">Icterícia</h1>
 
                 <div className="banner">
-                    <img src="https://ativosaude.akamaized.net/wp-content/uploads/2018/08/22072731/pele-amarelada-min.jpg" />
+                    <div className="banner-oque">
+                        <img src="https://ativosaude.akamaized.net/wp-content/uploads/2018/08/22072731/pele-amarelada-min.jpg" />
+                        <p>
+                        O que é icterícia?
+        De acordo com o gastroenterologista Rodrigo Toledo, formado pela Universidade Federal do Paraná (UFPR), o amarelão acontece pelo excesso do pigmento bilirrubina, que se fixa na pele gradativamente de modo a comprometer sua coloração natural. “O acometimento é inofensivo, mas visto como um sinal importante para outros diagnósticos”, ressalta.
+                        </p>
+                    </div>
+                    <div className="banner-desc">
+                        <img src="https://ativosaude.akamaized.net/wp-content/uploads/2018/08/22072546/ictericia-no-corpo-min.jpg" />
+                        <p>
+                            Bilirrubina é um pigmento amarelo produzido quando as hemácias, responsáveis por levar oxigênio às células, são deterioradas em um processo natural de reciclagem. Essa ação acontece em diferentes partes do corpo, como baço, medula óssea e fígado, sendo a última a mais comum.
+                            <br />
+                            Em seu estado normal, a substância amarelada vai até o fígado, onde se mistura com a bile, e é processada por meio das vias biliares, sendo utilizada posteriormente no processo da digestão e na urina.
+                        </p>
+                    </div>
                 </div>
-                <p>
-                    O que é icterícia?
-    De acordo com o gastroenterologista Rodrigo Toledo, formado pela Universidade Federal do Paraná (UFPR), o amarelão acontece pelo excesso do pigmento bilirrubina, que se fixa na pele gradativamente de modo a comprometer sua coloração natural. “O acometimento é inofensivo, mas visto como um sinal importante para outros diagnósticos”, ressalta.
-                    </p>
-
-                <div className="banner">
-                    <img src="https://ativosaude.akamaized.net/wp-content/uploads/2018/08/22072546/ictericia-no-corpo-min.jpg" />
-                    <p>
-                        Bilirrubina é um pigmento amarelo produzido quando as hemácias, responsáveis por levar oxigênio às células, são deterioradas em um processo natural de reciclagem. Essa ação acontece em diferentes partes do corpo, como baço, medula óssea e fígado, sendo a última a mais comum.
-                        <br />
-                        Em seu estado normal, a substância amarelada vai até o fígado, onde se mistura com a bile, e é processada por meio das vias biliares, sendo utilizada posteriormente no processo da digestão e na urina.
-                </p>
                     <h1 className="sobre-title">Tipos de icterícia</h1>
 
                     Existem quatro tipos de icterícia: a hemolítica, a hiperbilirrubinemia, <h2>hepatocelular e obstrutiva.</h2>
@@ -44,12 +46,10 @@ class Sobre extends Component {
                     <br />
                     Obstrutiva
                     Ocorre pela obstrução dos ductos biliares, possível consequência da presença de colangite e cânceres que impedem o fluxo da bilirrubina.
-            </div>
                 <a href="https://www.ativosaude.com/saude/ictericia/" >
                     Link de consulta.
                 </a>
             </div>
-
         );
     }
 }
